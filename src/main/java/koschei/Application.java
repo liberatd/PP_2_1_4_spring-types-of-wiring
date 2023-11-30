6e5f695a7f1389845ce919177c2e3afe33243d3e
Oleg Vologin <146234675+liberatd@users.noreply.github.com>
2023-12-01 01:28:09 +0400

    Update

--- src/main/java/koschei/Application.java
+++ src/main/java/koschei/Application.java
@@ -8,8 +8,12 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
+
         KoscheiTheDeathless koscheiTheDeathless =
                 applicationContext.getBean(KoscheiTheDeathless.class);
+
+
         System.out.println(koscheiTheDeathless.getRulesByDeth());
     }
 }
